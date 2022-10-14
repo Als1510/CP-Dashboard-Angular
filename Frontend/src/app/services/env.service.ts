a5a87ea05f777abfbb01e32c6ad9e6ca7508c79b
Als1510 <lekhrajsharmaas@gmail.com>
2022-10-14 11:47:11 +0530

    feat: Change Backend hosting server

--- Frontend/src/app/services/env.service.ts
+++ Frontend/src/app/services/env.service.ts
@@ -4,9 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EnvService {
-
   // Mongo_API_URL = 'http://localhost:5000/api'
-  Mongo_API_URL = 'https://competitivepdashboard.herokuapp.com/api'
+  // Mongo_API_URL = 'https://competitivepdashboard.herokuapp.com/api'
+  Mongo_API_URL = 'https://cp-dashboard-backend.onrender.com/api'
   Contest_API_URL = 'https://kontests.net/api/v1/all';
   User_API = 'https://cpd-user-api.herokuapp.com/api';
   constructor() { }
