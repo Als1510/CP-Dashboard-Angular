91a28fdd647aab215ba0862ad5d11e7d7055c513
Anshul Sharma <anshul.sharma@alectio.com>
2023-12-11 14:55:40 +0530

    fix: Backend URL

--- Frontend/src/app/services/env.service.ts
+++ Frontend/src/app/services/env.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EnvService {
-  Mongo_API_URL = 'http://localhost:5000/api'
-  // Mongo_API_URL = 'https://cp-dashboard-backend.onrender.com/api'
+  // Mongo_API_URL = 'http://localhost:5000/api'
+  Mongo_API_URL = 'https://cp-dashboard-backend.onrender.com/api'
   User_API = 'https://cpd-user-api.onrender.com/api';
   constructor() { }
 }
